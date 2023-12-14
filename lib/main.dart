9f82996d6ea8140126ca7355b49225be62252ed4
Amjad Abujamous <amjadmh73@gmail.com>
2023-12-14 15:38:00 +0300

    block widget and switch in patterns

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class DocumentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final (title, modified: modified) = document.metadata;
+    final blocks = document.getBlocks();
 
     return Scaffold(
       appBar: AppBar(
@@ -40,7 +41,45 @@ class DocumentScreen extends StatelessWidget {
           Center(
             child: Text('Last modified at $modified'),
           ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: blocks.length,
+              itemBuilder: (context, index) {
+                return BlockWidget(block: blocks[index]);
+              },
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class BlockWidget extends StatelessWidget {
+  final Block block;
+
+  const BlockWidget({
+    required this.block,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle;
+    switch (block.type) {
+      case 'h1':
+        textStyle = Theme.of(context).textTheme.displayMedium;
+      case 'p' || 'checkbox':
+        textStyle = Theme.of(context).textTheme.bodyMedium;
+      case _:
+        textStyle = Theme.of(context).textTheme.bodySmall;
+    }
+
+    return Container(
+      margin: const EdgeInsets.all(8),
+      child: Text(
+        block.text,
+        style: textStyle,
       ),
     );
   }
